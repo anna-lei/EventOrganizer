7ae558db5c7a7045ba4b809ba2450269780a2b9d
anna-lei <annaleinkenjost@web.de>
2017-03-13 12:27:58 +0100

    PageRenderingService ergänzt

--- EventOrganizer/src/main/java/de/auc/services/PageRenderingService.java
+++ EventOrganizer/src/main/java/de/auc/services/PageRenderingService.java
@@ -14,10 +14,25 @@ public class PageRenderingService {
 	private static final String PAGE_REGISTER_JSF = "register.jsf";
 	private static final String PAGE_LOGIN_JSF = "login.jsf";
 	private static final String PAGE_HOME_JSF = "home.jsf";
+	private static final String PAGE_MYEVENTS_JSF = "myEvents.jsf";
+	private static final String PAGE_MANAGER_RESERVATION_JSF = "managerReservierung.jsf";
+	private static final String PAGE_MANAGER_DETAIL_JSF = "managerDetail.jsf";
 	
 	@ManagedProperty("#{eventService}")
 	private EventService eventService;
 
+	public String getManagerDetail(){
+		return PAGE_MANAGER_DETAIL_JSF;
+	}
+	
+	public String getManagerReservation(){
+		return PAGE_MANAGER_RESERVATION_JSF;
+	}
+	
+	public String getMyEvents() {
+		return PAGE_MYEVENTS_JSF;
+	}
+	
 	public String getHome() {
 		return PAGE_HOME_JSF;
 	}
