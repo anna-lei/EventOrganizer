7f9d6b5733df3362f06c60e043955f1988239c19
anna-lei <annaleinkenjost@web.de>
2017-03-28 13:29:03 +0200

    erster Versuch

--- EventOrganizer/src/main/java/de/auc/model/User.java
+++ EventOrganizer/src/main/java/de/auc/model/User.java
@@ -11,8 +11,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 /**
  * Implementierung der Entität User.
